5622cb4e9f2329275be19d447b4971b389c05e40
mialivola <mialivolarandrianaivo@gmail.com>
2025-10-14 14:27:57 +0300

    amélioration page d'accueil

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Accueil } from './pages/accueil/accueil';
+import { Projets } from './pages/projets/projets';
+import { Contact } from './pages/contact/contact';
+import { Header } from './layout/header/header';
+import { Sidebar } from './layout/sidebar/sidebar';
+import { Footer } from './layout/footer/footer';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Accueil,
+    Projets,
+    Contact,
+    Header,
+    Sidebar,
+    Footer
   ],
   imports: [
     BrowserModule,
--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Accueil } from './pages/accueil/accueil';
+import { Projets } from './pages/projets/projets';
+import { Contact } from './pages/contact/contact';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: Accueil },
+  { path: 'projets', component: Projets },
+  { path: 'contact', component: Contact },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
